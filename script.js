cfe6085a1a845c765bbacee2cdcb17c36121a2ec
Edward Roddriguez <edwardR36@hotmail.com>
2019-12-19 18:19:56 -0500

    Add line strike-through toggle on item click

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ var div = document.querySelector('div');
 var input = document.getElementById('userInput');
 var button = document.getElementsByTagName('button')[0];
 var ul = document.querySelector('ul');
+// var list = document.querySelectorAll('li');
 var warning = document.getElementById('warningLabel');
 var groceryList = [
   'Jello',
@@ -42,25 +43,26 @@ var groceryList = [
   'Notebook'
 ];
 
-//Changing class names of an element example
+//  Changing class names of an element example
 div.classList.add('hit-the-floor');
 
-//output 'click' on button click
+//  output 'click' on button click
 button.addEventListener('click', function() {
   console.log('click');
 });
 
-//output 'enter' on mouse entering button
+//  output 'enter' on mouse entering button
 button.addEventListener('mouseenter', function() {
   console.log('enter');
 });
 
 function removeWhiteSpace(text) {
-  //regex for removing whitespace
+  //  regex for removing whitespace
   return text.replace(/\s+/g, '');
 }
 
-//check if input value is valid an non-empty
+/*  check if input value is valid an non-empty
+display warning if already on the list*/
 function inputIsValid() {
   var inputVal = removeWhiteSpace(input.value);
   if (inputVal.length > 0)
@@ -72,7 +74,7 @@ function inputIsValid() {
   else return false;
 }
 
-//create list element
+//  create a list element under ul
 function createListElement(item) {
   var li = document.createElement('li');
   li.appendChild(document.createTextNode(item));
@@ -82,12 +84,15 @@ function createListElement(item) {
   warning.className = 'warningLabelOFF';
 }
 
+/*************************  EVENT LISTENERS **************************/
+//  create an element on button click and add to list and groceryList
 function addListAfterClick() {
   if (inputIsValid()) {
     createListElement(removeWhiteSpace(input.value));
   }
 }
 
+//  create an element on enter key press and add to list and groceryList
 function addListAfterKeyPress(event) {
   var inputVal = input.value.replace(/\s+/g, '');
   if (event.keyCode === 13 && inputIsValid()) {
@@ -95,8 +100,16 @@ function addListAfterKeyPress(event) {
   }
 }
 
-//create an element on button click and add to list and groceryList
+//  toggle strike-through on item click
+function toggleAfterClick(e) {
+  e.target.classList.toggle('done');
+}
+
+//  add mouse click event listener to add button
 button.addEventListener('click', addListAfterClick);
 
-//create an element on enter key press and add to list and groceryList
+//  add enter key event listener to input field
 input.addEventListener('keypress', addListAfterKeyPress);
+
+//  add mouse click listener to toggle line strikethrough on item
+ul.addEventListener('click', toggleAfterClick);
