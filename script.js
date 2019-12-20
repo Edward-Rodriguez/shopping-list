90355ae30e8f00d9735999bc4305ef48db6ace7a
Edward Roddriguez <edwardR36@hotmail.com>
2019-12-20 15:09:45 -0500

    Add delete button to each list Set button to delete item Toggle trash image on hover

--- script.js
+++ script.js
@@ -32,16 +32,24 @@ var div = document.querySelector('div');
 var input = document.getElementById('userInput');
 var button = document.getElementsByTagName('button')[0];
 var ul = document.querySelector('ul');
-// var list = document.querySelectorAll('li');
+var deleteButtons = document.querySelectorAll('input[name="delete"]');
 var warning = document.getElementById('warningLabel');
 var groceryList = [
   'Jello',
-  'SPinach',
+  'Spinach',
   'Rice',
   'Birthday Cake',
   'Candles',
   'Notebook'
 ];
+const deleteImage = 'images/delete.png';
+const deleteImageRed = 'images/delete_red.png';
+const attributes = {
+  type: 'image',
+  name: 'delete',
+  alt: 'Delete',
+  src: deleteImage
+};
 
 //  Changing class names of an element example
 div.classList.add('hit-the-floor');
@@ -74,9 +82,21 @@ function inputIsValid() {
   else return false;
 }
 
-//  create a list element under ul
+//  set attributes for input button
+function setAttributes(element) {
+  for (var key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+//  create a list element under ul with a delete button and new item
 function createListElement(item) {
   var li = document.createElement('li');
+  var deleteButton = document.createElement('input');
+  deleteButton.addEventListener('click', deleteItem);
+  deleteButton.addEventListener('mouseenter', toggleTrashImage);
+  setAttributes(deleteButton);
+  li.appendChild(deleteButton);
   li.appendChild(document.createTextNode(item));
   ul.appendChild(li);
   groceryList.push(item);
@@ -84,7 +104,6 @@ function createListElement(item) {
   warning.className = 'warningLabelOFF';
 }
 
-/*************************  EVENT LISTENERS **************************/
 //  create an element on button click and add to list and groceryList
 function addListAfterClick() {
   if (inputIsValid()) {
@@ -94,7 +113,6 @@ function addListAfterClick() {
 
 //  create an element on enter key press and add to list and groceryList
 function addListAfterKeyPress(event) {
-  var inputVal = input.value.replace(/\s+/g, '');
   if (event.keyCode === 13 && inputIsValid()) {
     createListElement(removeWhiteSpace(input.value));
   }
@@ -105,11 +123,39 @@ function toggleAfterClick(e) {
   e.target.classList.toggle('done');
 }
 
+// delete parent list element containing delete button
+function deleteItem(e) {
+  var targetParent = e.target.parentNode; // parent list element
+  var itemToBeRemoved = targetParent.innerText;
+  targetParent.parentNode.removeChild(targetParent);
+  groceryList.splice(groceryList.indexOf(itemToBeRemoved), 1);
+}
+
+// change trash can image to red
+function toggleTrashImage(e) {
+  var element = e.target;
+  if (element.getAttribute('src') === deleteImage)
+    element.setAttribute('src', deleteImageRed);
+  else element.setAttribute('src', deleteImage);
+}
+
+/*************************  EVENT LISTENERS **************************/
 //  add mouse click event listener to add button
 button.addEventListener('click', addListAfterClick);
 
 //  add enter key event listener to input field
 input.addEventListener('keypress', addListAfterKeyPress);
 
-//  add mouse click listener to toggle line strikethrough on item
+//  add mouse click listener to toggle line strike-through on item
 ul.addEventListener('click', toggleAfterClick);
+
+// add mouse click listener to each delete button
+deleteButtons.forEach(button => {
+  button.addEventListener('click', deleteItem);
+  button.addEventListener('mouseenter', toggleTrashImage);
+  button.addEventListener('mouseleave', toggleTrashImage);
+});
+
+// listener to change trash can to red on hover
+
+/******************** END OF EVENT LISTENERS *************************/
